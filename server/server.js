444c4da5046b6a4dee65764224aa275bc313853b
RobertUlloa <roanuc8@gmail.com>
2024-10-10 18:31:22 -0500

    fixing server

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const { ApolloServer } = require('@apollo/server');
 const { expressMiddleware } = require('@apollo/server/express4');
 const path = require('path');
-const db = require('./config/connection');
+const mongoose = require('./config/connection'); 
 const { typeDefs, resolvers } = require('./schemas');
 
 const app = express();
@@ -14,26 +14,33 @@ const server = new ApolloServer({
 });
 
 const startApolloServer = async () => {
-  await server.start();
+  try {
+    await server.start();
 
-  app.use(express.urlencoded({ extended: true }));
-  app.use(express.json());
+    //  middlewares
+    app.use(express.urlencoded({ extended: true }));
+    app.use(express.json());
 
-  app.use('/graphql', expressMiddleware(server));
+    // GraphQL route
+    app.use('/graphql', expressMiddleware(server));
 
-  if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../client/build')));
-    app.get('*', (req, res) => {
-      res.sendFile(path.join(__dirname, '../client/build/index.html'));
-    });
-  }
+    // Serve static assets if in production
+    if (process.env.NODE_ENV === 'production') {
+      app.use(express.static(path.join(__dirname, '../client/build')));
+      app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, '../client/build/index.html'));
+      });
+    }
 
-  db.once('open', () => {
+    await mongoose.connection;
+
     app.listen(PORT, () => {
       console.log(`API server running on port ${PORT}!`);
       console.log(`GraphQL at http://localhost:${PORT}/graphql`);
     });
-  });
+  } catch (err) {
+    console.error('Error starting the server:', err);
+  }
 };
 
 startApolloServer();
